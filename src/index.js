69a1bcd0a23a644aebc4627ff9e68a31e07e1e13
Mohammed Raja <mohammed2raja@gmail.com>
2020-02-24 13:25:25 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import "./styles.css";
 
-const inBrowser = typeof window !== 'undefined'
+const inBrowser = typeof window !== "undefined";
 
 // get user agent
-const UA = inBrowser && window.navigator.userAgent.toLowerCase()
+const UA = inBrowser && window.navigator.userAgent.toLowerCase();
 
 // detect browser
-const isIE = UA && /msie|trident/.test(UA)
-const isIE9 = UA && UA.indexOf('msie 9.0') > 0
-const isEdge = UA && UA.indexOf('edge/') > 0
-const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
-const isPhantomJS = UA && /phantomjs/.test(UA)
-const isFF = UA && UA.match(/firefox\/(\d+)/)
+const isIE = UA && /msie|trident/.test(UA);
+const isIE9 = UA && UA.indexOf("msie 9.0") > 0;
+const isEdge = UA && UA.indexOf("edge/") > 0;
+const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge;
+const isPhantomJS = UA && /phantomjs/.test(UA);
+const isFF = UA && UA.match(/firefox\/(\d+)/);
 
 // detect OS
-const isAndroid = UA && UA.indexOf('android') > 0
-const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA)
-
+const isAndroid = UA && UA.indexOf("android") > 0;
+const isIOS = UA && /iphone|ipad|ipod|ios/.test(UA);
 
 document.getElementById("app").innerHTML = `
 <h1>Browser checking</h1>
